40b82b32e22e56e06b08ef011eb3ccb3a11a6856
Alya Rysda RAmadhanti <alyarysdaramadhanti@gmail.com>
2019-05-21 13:53:36 +0700

    Datasaurus

--- datasaurus.R
+++ datasaurus.R
@@ -1,23 +1,23 @@
 library(tidyverse)
-install.packages("datasauRus")
 library(datasauRus)
 
 datasaurus_dozen
 
 datasaurus_dozen %>%
   group_by(dataset) %>%
-  summarise(
+  summarize(
     mean_x = mean(x),
     sd_x = sd(x),
-    mean_y = mean (y),
+    mean_y = mean(y),
     sd_y = sd(y),
-    cor = cor (x,y)
+    cor = cor(x,y)
   )
 
 datasaurus_dozen %>%
-  ggplot(aes(x = x, y =y)) +
-  geom_point(aes(colour = dataset)) +
-  geom_smooth(method = "lm", se = FALSE ) +
-  facet_wrap(~dataset, ncol = 3, scales = "free") +
-  guides(colour = FALSE ) +
+  ggplot(aes(x = x, y = y)) +
+  geom_point(aes(colour=dataset)) +
+  geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap(~dataset, ncol = 3, scales = "free")+
+  guides(colour=FALSE)+
   theme_minimal()
+
